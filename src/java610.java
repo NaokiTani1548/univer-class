0baf17942a4e4d6cd9179a25fab1587b55251066
Yuki Tani <brave.bright.0604@gmail.com>
2022-06-17 01:40:31 +0900

    beutiful code

--- src/java610.java
+++ src/java610.java
@@ -3,81 +3,94 @@ import java.util.Random;
 
 public class java610 {
 
-		public static void main(String[] args) {
-			Scanner stdIn =new Scanner(System.in);
-			Random rand = new Random();
-			
-			String[] hands = {"グー","チョキ","パー"};
-			int retry =0; int x; int y; int z;
-			do {
-				System.out.print("コンピュータの手の数");
-				int n = stdIn.nextInt();
-				int[]comp = new int[n];
-				for(int i = 0; i < n ; i++) {
-					comp[i] = rand.nextInt(3);
-				}
-				int user;
-				do {
-					x =0; y=0; z=0;
-					System.out.print("じゃんけんポン\n");
-					for(int i = 0; i < 3 ; i++) {
-						System.out.printf("(%d)%s",i,hands[i]);
-				    }
-					System.out.print(":");
-				    user=stdIn.nextInt();
-			    }while(user<0||user>2);
-			   for(int i = 0; i < n ; i++) {	
-				  System.out.print("com"+i+"が"+hands[comp[i]]+" ");
-				 }System.out.print("あなたがが"+hands[user]);
-				 int judge = 0;
-				
-				switch(user) {
-				 case 0: for(int i = 0; i < n ; i++) {
-						   if(comp[i] != 0)   x = x+1;
-					   }if(x == 0) { judge = 0; break;}
-				        for(int j = 0 ; j < n ; j++) {
-				         if(comp[j] == 2) y = y+1;
-				        } if(y == 0) {judge = 2; break;}
-				       for(int l = 0; l < n ; l++) {
-				    	   if(comp[l] == 1) z = z+1; 
-				       }if(z == 0) {judge = 1; break;}
-				         judge = 0; break;
-				 case 1:  for(int i = 0; i < n ; i++) {
-					      if(comp[i] != 1)  x = x+1;
-				       }if(x==0)  { judge=0; break;}
-			           for(int j = 0; j < n ; j++) {
-			    	     if(comp[j] == 0) y = y+1; 
-			           }if(y == 0) { judge = 2; break;}
-			           for(int l = 0; l < n ; l++) {
-			    	     if(comp[l] == 2) z = z+1; 
-			           }if(z == 0) { judge = 1; break;}
-			              judge = 0; break;    
-				 case 2: for(int i = 0; i < n ; i++) {
-					      if(comp[i] != 2)  x = x+1;
-				        }if(x == 0)  { judge = 0; break;}
-			            for(int j = 0; j < n ; j++) {
-			    	      if(comp[j] == 1) y = y+1; 
-			             }if(y == 0) { judge = 2; break;}
-			             for(int l = 0; l < n ; l++) {
-			    	      if(comp[l] == 0) z = z+1; 
-			             }if(z == 0) { judge = 1; break;}
-			                judge = 0; break;
-				}
-				switch(judge) {
-				case 0: System.out.println("\n引き分けです");break;
-				case 1: System.out.println("\nあなたの負けです");break;
-				case 2: System.out.println("\nあなたの勝ちです");
-				
-				}
-				do {
-					System.out.print("もう一度？０・・・いいえ　１・・・はい");
-					retry =stdIn.nextInt();
-				}while(retry != 0 && retry != 1);
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		Random rand = new Random();
 
-		}while(retry==1);
-			
-  }
+		while (true) {
+			battle(stdIn, rand);
+			boolean isTryAgain = askTryAgain(stdIn);
+			if (!isTryAgain) return;
+		}
+  	}
+
+	private static void battle(Scanner stdIn, Random rand) {
+		int compCount = 0;
+
+		// Step 1: decide number of computers
+		while (compCount <= 0) {
+			System.out.print("コンピュータの手の数: ");
+			try {
+				compCount = stdIn.nextInt();
+			} catch (Exception e) {
+				stdIn.next(); // if input is not integer, skip it
+			}
+		}
+
+		// Step 2: decide hands of computers
+		int[] compHands = new int[compCount];
+		for (int i = 0; i < compHands.length; i++) {
+			compHands[i] = rand.nextInt(3);
+		}
+
+		// Step 3: decide user's hand
+		String[] handNames = {"グー", "チョキ", "パー"};
+
+		int userHand = -1;
+		while (userHand < 0 || 2 < userHand) {
+			System.out.println("じゃんけんポン");
+			for(int i = 0; i < 3; i++) {
+				System.out.printf("%d : %s\n", i, handNames[i]);
+			}
+			System.out.print("> ");
+			try {
+				userHand = stdIn.nextInt();
+			} catch (Exception e) {
+				stdIn.next(); // if input is not integer, skip it
+			}
+		}
+
+		// Step 6: judge result
+		int winHand = (userHand + 1) % 3;
+		int loseHand = (userHand + 2) % 3;
+		boolean isWin = false;
+		boolean isLose = false;
+		for (int i = 0; i < compHands.length; i++) {
+			if (compHands[i] == winHand) {
+				isWin = true;
+			} else if (compHands[i] == loseHand) {
+				isLose = true;
+			}
+		}
+
+		// Step 5: show result
+		System.out.println("=== 結果 ===");
+		for (int i = 0; i < compCount; i++) {	
+			System.out.println("com " + (i + 1) + " が " + handNames[compHands[i]]);
+		}
+		System.out.println("あなた が " + handNames[userHand]);
+		System.out.println();
+		
+		if (isWin && !isLose) {
+			System.out.println("あなたの勝ちです");
+		} else if (!isWin && isLose) {
+			System.out.println("あなたの負けです");
+		} else {
+			System.out.println("引き分けです");
+		}
+	}
+
+	private static boolean askTryAgain(Scanner stdIn) {
+		int answer = -1;
+		while (answer != 0 && answer != 1) {
+			System.out.println("もう一度？ ０: いいえ １: はい");
+			System.out.print("> ");
+			try {
+				answer = stdIn.nextInt();
+			} catch (Exception e) {
+				stdIn.next(); // if input is not integer, skip it
+			}
+		}
+		return answer == 1;
+	}
 }
-
-
-
