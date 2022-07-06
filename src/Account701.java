10f29faddacae78af1edeb6f077de06fc1d0fff8
Yuki Tani <brave.bright.0604@gmail.com>
2022-07-06 22:41:06 +0900

    create account safer

--- src/Account701.java
+++ src/Account701.java
@@ -1,34 +1,36 @@
-class Account701 {
-	String name;
-	String no;
-	long balance;
+public class Account701 {
 	public static void main(String[] args) {
+		Account account = new Account("足立幸一", "123456", 1000);
+		account.showAccountInfo(); // 足立幸一
 		
+		account.setName("田中幸一");
+		account.showAccountInfo(); // 田中幸一
+	}
+}
+
+class Account {
+	private String userName;
+	private String userId;
+	private long balance;
+
+	public Account(String userName, String userId, long balance) {
+		// コンストラクタは初期化。変なAccountが作成されないように、本当はここで、ユーザーネームやIDが有効か（空でないか）、
+		// 預金残高が有効か（マイナスでないか）などをチェックしたりする。今回は省略
+		this.userName = userName;
+		this.userId = userId;
+		this.balance = balance;
 	}
 
-
-	Account701(String n, String num, long z) {
-		name = n;
-		no = num;
-		balance = z;
+	public void setName(String newName) {
+		// 同じく、有効な名前かどうかのチェックをここで本当はしたりする。
+		this.userName = newName;
 	}
 
-	void setName(String x) {
-		name = x;
-
-	}
-
-	class AccountTester {
-		public static void main(String[] args) {
-
-			Account701 tanaka = new Account701("足立幸一", "123456", 1000);
-
-			tanaka.setName("田中幸一");
-
-			System.out.println("口座");
-			System.out.println("口座名義" + tanaka.name);
-			System.out.println("口座番号" + tanaka.no);
-			System.out.println("預金残高" + tanaka.balance);
-		}
+	public void showAccountInfo() {
+		System.out.println();
+		System.out.println("口座");
+		System.out.println("口座名義 " + this.userName);
+		System.out.println("口座番号 " + this.userId);
+		System.out.println("預金残高 " + this.balance);
 	}
 }
